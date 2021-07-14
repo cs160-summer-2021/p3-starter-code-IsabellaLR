7f2f07837548203f43b806af3804079b5b4a8d6c
elletran <ellettran@gmail.com>
2021-07-14 10:42:34 -0700

    library

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.index, name='new_interaction'),
+    path('social', views.social, name='social'),
+    path('competitions', views.competitions, name='competitions'),
+     path('library', views.library, name='library')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def social(request):
+    return render(request, 'coloring/social.html')
+
+def competitions(request):
+    return render(request, 'coloring/competitions.html')
+
+def library(request):
+    return render(request, 'coloring/library.html')
